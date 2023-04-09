0d8c4aadb364cdfd1a5641530ed9980e966a03a8
Jeffrey <jeffreygrospe@yahoo.com>
2023-04-09 19:25:47 -0400

    add print rows and winnings
    - also added documentation

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # slot-maching
- Javascript Project
+- Javascript Project
 
+# Description
+- A CLI slot machine game built using javascript
+
+# Technologies
+- JavaScript
+- Nodejs
 
 # packages
 - to install required packages, navigate to the root folder and run
 - `npm install`
+
+# Reference
+- Learn JavaScript with This one Project
+- [Tech With Team](https://www.youtube.com/@TechWithTim)
--- project.js
+++ project.js
@@ -86,11 +86,78 @@ const spin = () => {
         }
     }
     return reels;
+};
+
+const transpose = (reels) => {
+    const rows = [];
+    for (let i = 0; i < ROWS; i++){
+        rows.push([]);
+        for (let j = 0; j < COLS; j++) {
+            rows[i].push(reels[j][i]);
+        }
+    }
+    return rows;
 }
-spin();
-// let balance = deposit();
-// console.log(balance);
-// const numberOfLines = getNumberOfLines();
-// console.log(numberOfLines);
-// const bet = getBet(balance, numberOfLines);
-// console.log(bet);
+
+const printRows = (rows) => {
+    for (const row of rows) {
+        let rowString = "";
+        for (const [i, symbol] of row.entries()) {
+            rowString += symbol;
+            if (i != row.length - 1) {
+                rowString += " | "
+            }
+        }
+        console.log(rowString);
+    }
+};
+
+const getWinnings = (rows, bet, lines) => {
+    let winnings = 0;
+    for (let row = 0; row < lines; row++) {
+        const symbols = rows[row];
+        let allSame = true;
+
+        for (const symbol of symbols) {
+            if (symbol != symbols[0]) {
+                allSame = false;
+                break;
+            }
+        }
+
+        if (allSame) {
+            winnings += bet * SYMBOLS_VALUES[symbols[0]];
+        }
+    }
+    return winnings;
+};
+
+const game = () => {
+
+    let balance = deposit();
+    while (true){
+        console.log("You have a balance of $" + balance);
+        const lines = getNumberOfLines();
+        const bet = getBet(balance, lines);
+        balance -= bet * lines;
+        const reels = spin();
+        const rows = transpose(reels);
+        printRows(rows);
+        const winnings = getWinnings(rows, bet, lines);
+        balance += winnings;
+        console.log("You won, $" + winnings.toString());
+
+        if (balance <= 0) {
+            console.log("You ran ot of money!");
+            break;
+        }
+        const playAgain = prompt("Do you want to play again (y/n)?")
+
+        if (playAgain != "y") {
+            console.log("Thank you for playing!, see you next time");
+            break;
+        }
+    }
+}
+
+game();
